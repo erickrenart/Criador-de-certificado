e040dd9420a70ea913ac0e0cc975d0ad72c0dc63
erickrenart <163436545+erickrenart@users.noreply.github.com>
2025-03-20 08:51:33 -0300

    Update bala.js

--- bala.js
+++ bala.js
@@ -1,1 +1,13 @@
-const form = document.getElementById("form");
+function atualizartexto(){
+    const nome= document.getElementById("nome").value;
+    const cpf= document.getElementById("cpf").value;
+    const curso= document.getElementById("curso").value;
+    const duracao= document.getElementById("duracao").value;
+
+    document.getElementById("pao1").innerText= nome;
+    document.getElementById("pao2").innerText= cpf;
+    document.getElementById("pao3").innerText= curso;
+    document.getElementById("pao4").innerText= duracao;
+}
+
+
